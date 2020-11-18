bf8a78d91419d94251211044a5cfeef74266b747
Kevin Sivabalan <3488288+timestep@users.noreply.github.com>
2020-11-18 09:40:27 -0500

    lean Up

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { PokemonTypes } from './queries/PokemonTypes';
 
 import { styleReset } from 'react95';
-// original Windows95 font (optionally)
-// tslint:disable-next-line:no-submodule-imports
-// import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-// tslint:disable-next-line:no-submodule-imports
-// import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-// pick a theme of your choice
 // tslint:disable-next-line:no-submodule-imports
 import original from 'react95/dist/themes/original';
 import { Main } from './container/Main';
